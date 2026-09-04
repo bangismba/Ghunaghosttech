fc6b7e128f0fd8b0aadbb90321a9e5d0de15cf60
bangismba <amb2004mba@gmail.com>
2026-09-04 13:10:57 +0100

    update

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-25613826'], (function (workbox) { 'use strict';
     "revision": "d41d8cd98f00b204e9800998ecf8427e"
   }, {
     "url": "index.html",
-    "revision": "0.9gp2v2drfro"
+    "revision": "0.t09t4k0p8vg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
